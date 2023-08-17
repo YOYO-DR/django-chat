0a3acb8724bd89e07212c4bb9236c8dd99d1e483
Yoiner Duran <duranyoiner@gmail.com>
2023-08-17 11:34:41 -0500

    chat v3 - routing wss

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
-
+import os
 from . import consumers
 
 websocket_urlpatterns = [
   re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
+if "WEBSITE_HOSTNAME" in os.environ:
+    websocket_urlpatterns.append(re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),)
