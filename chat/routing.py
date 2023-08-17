bae3cf4176d6dc372b35bba65790b6b32912ad40
Yoiner Duran <duranyoiner@gmail.com>
2023-08-17 11:53:17 -0500

    chat v3 - routing wss

--- chat/routing.py
+++ chat/routing.py
@@ -6,4 +6,4 @@ websocket_urlpatterns = [
   re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
 if "WEBSITE_HOSTNAME" in os.environ:
-    websocket_urlpatterns.append(re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),)
+    websocket_urlpatterns.append(re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()))
