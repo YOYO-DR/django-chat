65f170152b16425933743f605d23c2fed08d5276
Yoiner Duran <duranyoiner@gmail.com>
2023-08-19 19:51:23 -0500

    iniciando con los modelos del chat

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class HistorialChat(models.Model):
+  user=models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="Usuario")
+  message=models.TextField(null=True, blank=True, verbose_name="Mensaje")
+  datetime=models.DateTimeField(auto_now_add=True, verbose_name="Fecha enviado")
