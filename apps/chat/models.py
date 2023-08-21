476ecdb584ad665b57032cc308c31e041ec58a31
Yoiner Duran <duranyoiner@gmail.com>
2023-08-20 23:59:32 -0500

    historial de chat integrado con peticiones post

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import model_to_dict
 
 class HistorialChat(models.Model):
   user=models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="Usuario")
   message=models.TextField(null=True, blank=True, verbose_name="Mensaje")
-  datetime=models.DateTimeField(auto_now_add=True, verbose_name="Fecha enviado")
+  # formato: 'YYYY-MM-DD HH:MM:SS'
+  datetime=models.DateTimeField(verbose_name="Fecha enviado")
+
+  def __str__(self):
+    return self.message
+
+  def toJSON(self):
+    item=model_to_dict(self)
+    item['user'] = {"id":self.user.id, "username":self.user.username}
+    return item
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,6 +1,10 @@
+from django.http import JsonResponse
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+import json
+from .models import HistorialChat
+from django.contrib.auth.models import User
 
 class InicioView(TemplateView):
     template_name="chat.html"
@@ -10,8 +14,23 @@ class InicioView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
     
 
+    def post(self, request, *args, **kwargs):
+        data=json.loads(request.body)
+        try:
+          user=User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+          return JsonResponse({"error":"Usuario no existe"})
+        except Exception as e:
+           return JsonResponse({"error":str(e)})
+
+        HistorialChat(user=user,message=data['message'],datetime=data['datetime']).save()
+
+        return JsonResponse({"ok":"ok"})
+    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["room_name"] = "social"
+        context['historial']=[message.toJSON() for message in HistorialChat.objects.all().order_by("datetime")]
         return context
     
